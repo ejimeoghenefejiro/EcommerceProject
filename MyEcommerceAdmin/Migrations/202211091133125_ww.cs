8c41feff7cbc7ea72efcf284d9914c121ec3581e
Ejime Macdonald Oghenefejiro <ejimeoghenefejiro@yahoo.com>
2022-11-11 08:23:31 +0100

    Add project files.

---
+++ MyEcommerceAdmin/Migrations/202211091133125_ww.cs
@@ -0,0 +1,16 @@
+﻿namespace MyEcommerceAdmin.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ww : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
